41dae04330df542f537dd05f5ec0836db1ba18f1
Khankeldi Shakhmukhanbetov <khanekshakh@gmail.com>
2024-10-17 23:38:29 +0500

    Практика 7

---
+++ Practic7.js/Задача_1/ICostCalculationStrategy.java
@@ -0,0 +1,5 @@
+package Задача_1;
+
+public interface ICostCalculationStrategy {
+    double Calculator(int passenger, int TypeClass, boolean bag);
+}
---
+++ Practic7.js/Задача_1/Train.java
@@ -0,0 +1,20 @@
+package Задача_1;
+
+public class Train implements ICostCalculationStrategy{
+    @Override
+    public double Calculator(int passenger, int TypeClass, boolean bag) {
+        double cost = 0;
+        if (TypeClass == 1){
+            cost =  passenger * 1000;
+        }
+        else if (TypeClass == 2){
+            cost = passenger * 800;
+        }
+        else
+            cost = passenger * 600;
+        if (bag){
+            cost = cost * 2;
+        }
+        return cost;
+    }
+}
